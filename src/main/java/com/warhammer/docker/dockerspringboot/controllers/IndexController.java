be27996e0fc7185a4c04e1714e40826767c08ab5
Magnus Skalleberg Rom <magnus@proteria.com>
2019-09-18 23:51:13 +0200

    Fixed fly way DB

--- src/main/java/com/warhammer/docker/dockerspringboot/controllers/IndexController.java
+++ src/main/java/com/warhammer/docker/dockerspringboot/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.warhammer.docker.dockerspringboot.controllers;
 
 import com.warhammer.docker.dockerspringboot.services.UnitServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class IndexController {
 
     UnitServices unitServices;
 
+    @Autowired
     public IndexController(UnitServices unitServices){
         this.unitServices = unitServices;
     }
